6f8c23ca04c736f8b5f0781a39bc191a4dd1facb
Eren Findik <findike10@gmail.com>
2023-08-12 12:20:26 +0300

    RegisterController eklendii register teması ilgili sayfaya eklendi.

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Conceate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        {
+            if(ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = appUserRegisterDto.Username,
+                    Name = appUserRegisterDto.Name,
+                    Email = appUserRegisterDto.Email,
+                    Surname = appUserRegisterDto.Surname
+                };
+                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+            }
+            return View();
+        }
+    }
+}
