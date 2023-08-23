b89a6d50792e5a1f3f009cfe7d36caeee946012f
Eren Findik <findike10@gmail.com>
2023-08-23 19:57:17 +0300

    Hatalar türkçeye çevrildi.

---
+++ EasyCashIdentityProject.PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        //methodun işleyişini bozmadan o methodun işleyişini sizin istediğiniz formata çevirmek
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Paralo en az {length} karakter olmadılır"
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description ="Lütfen en az bir büyük harf giriniz"
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Lütfen en az bir alfa numerik karakter giriniz"
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "L" +
+                "tfen en az bir küçük harf kullanınız"
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Lütfen ez az bir tane rakam giriniz"
+            };
+        }
+    }
+}
--- EasyCashIdentityProject.PresentationLayer/Program.cs
+++ EasyCashIdentityProject.PresentationLayer/Program.cs
@@ -1,5 +1,6 @@
 using EasyCashIdentityProject.DataAccessLayer.Concreate;
 using EasyCashIdentityProject.EntityLayer.Conceate;
+using EasyCashIdentityProject.PresentationLayer.Models;
 using Microsoft.Win32;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>();
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>
+    ().AddErrorDescriber<CustomIdentityValidator>();
 
 var app = builder.Build();
 
